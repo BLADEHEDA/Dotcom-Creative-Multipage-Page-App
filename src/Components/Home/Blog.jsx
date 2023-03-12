4458eb709972ac102090335fcf397056fab63f45
Blade Heda <nkwetakembrunodudu9@gmail.com>
2023-03-12 19:30:17 +0100

    changes doneto blog section

--- src/Components/Home/Blog.jsx
+++ src/Components/Home/Blog.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import blog1 from "../images/blog-1.jpg"
 import blog2 from "../images/blog-2.jpg"
+import user1 from "../images/user.jpg"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faBookBookmark} from '@fortawesome/free-solid-svg-icons'
+// import { faBookBookmark } from "@fortawesome/free-regular-svg-icons"
+
+
+
 
 const BlogProp=(props)=>{
     return(
-        <main className="Blogprop">
+        <main className="Blogprop mb-[3em]">
             <section className="blogprop-first relative">
                <div className="blog-pic-div">
                <img src={props.src }alt="Blog-image" className="Blog-picdiv" />
@@ -14,14 +21,25 @@ const BlogProp=(props)=>{
                     <h1 className="blog-date-fig text-center text-[1.65em] font-[900] ">01</h1>
                 </div>
             </section>
-            <section className="blogprop-section">
-                <div className="blogprop-flexed">
-                    <div className="blogprop-item">
-                        <img src="" alt="" className="blogprop-icon" />
+            <section className="blogprop-section px-[2em] mt-[1.5em] ">
+                <div className="blogprop-flexed flex justify-betwee mb-[1em] ">
+                    <div className="blogprop-item w-[15%]">
+                        <img src={user1} alt="user-img " className="blogprop-icon rounded-[50%] " />
                     </div>
-                    <div className="blogprop-item"></div>
-                    <div className="blogprop-item"></div>
-                    <div className="blogprop-item"></div>
+                    <div className="blogprop-item mt-[.5em]"> 
+                   <a href="/"><li className="blogprop-link list-none text-[#6c757d]">John Doe</li></a> 
+                    </div>
+                    <div className="blogprop-item mt-[.5em]">
+                    <FontAwesomeIcon className='h-[1.5em]  text-[#FDAC53] bg-[#fff] ' icon={faBookBookmark}/>
+                    </div>
+                    <div className="blogprop-item mt-[.5em]">
+                    <a href="/"><li className="blogprop-link list-none text-[#6c757d] ">Web Design</li></a> 
+                    </div>
+                </div>
+                <div className="blogprop-content">
+                  <a className='link-blogprop font-[700] text-[1.25em] leading-6 ' href="/">Kasd tempor diam sea
+                   justo dolor kasd amet dolor
+                   labore amet clita est diam dolor</a>
                 </div>
             </section>
         </main>
@@ -38,6 +56,7 @@ const Blog = () => {
       </section>
       <section className="blog-second">
         <BlogProp src={blog1}/>
+        <BlogProp src={blog2}/>
       </section>
       
     </main>
