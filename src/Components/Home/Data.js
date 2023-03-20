68201eeb25928fb2fdcbcd9d38855c77f391a6ff
Blade Heda <nkwetakembrunodudu9@gmail.com>
2023-03-20 13:46:13 +0100

    added data

---
+++ src/Components/Home/Data.js
@@ -0,0 +1,14 @@
+const Data= [
+    {
+    background: require("../images/carousel-1.jpg"),
+    text:"Creative Services For Brands Grow"
+    },
+      {
+    background: require("../images/carousel-2.jpg"),
+    text:"Highly Professional Team Members"
+    },
+      {
+    background: require("../images/carousel-3.jpg"),
+    text:"Happy Clients & Positive Reviews"
+    }
+]
