fcfadbc6c9f446511cc66941b1b3ccd4529a380f
Blade Heda <nkwetakembrunodudu9@gmail.com>
2023-03-21 23:54:39 +0100

    new

--- src/Components/Shared/NavBar.jsx
+++ src/Components/Shared/NavBar.jsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 const NavBar = () => {
+  // write the logic to displa
 
   return (
-    <section className='Navvar'>
-        <navbar className="mobile-nav flex justify-between bg-[#131313] py-[0.3em] px-[1em] lg:hidden">  
+    <section className='Navvar z-[9999]'>
+        <navbar className=" mobile-nav flex justify-between bg-[#131313] py-[0.3em] px-[1em] lg:hidden">  
         <nav className="mobile-nav-left font-[900] text-[2.1em] text-slate-100">
              <span className="color-logo  ">D</span>ot<span className="color-logo">C</span>om
           </nav>
@@ -16,8 +17,8 @@ const NavBar = () => {
             <FontAwesomeIcon className='h-[1.75em] text-slate-300' icon={faBars }/></button>  
         </nav>
        </navbar>
-       <ul className="navlinks text-[#fff] font-[400] bg-[#131313]
-            text-[1em] lg:hidden ">
+       <ul className="navlinks text-[#fff] font-[400] bg-[#131313] text-[15px] 
+            md:text-[1.1em] lg:hidden">
               <li className="navlink">Home </li>
               <li className="navlink">About</li>
               <li className="navlink">Services</li>
