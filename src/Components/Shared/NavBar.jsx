ff7582addab61548497e0ef5ca5e5f110bf3b79b
Blade Heda <nkwetakembrunodudu9@gmail.com>
2023-03-21 23:10:09 +0100

    navbar in progress

--- src/Components/Shared/NavBar.jsx
+++ src/Components/Shared/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
 
   return (
     <section className='Navvar'>
-        <navbar className="mobile-nav flex justify-between bg-[#131313] py-[0.3em] px-[1em] lg:hidden"> 
+        <navbar className="mobile-nav flex justify-between bg-[#131313] py-[0.3em] px-[1em] lg:hidden">  
         <nav className="mobile-nav-left font-[900] text-[2.1em] text-slate-100">
              <span className="color-logo  ">D</span>ot<span className="color-logo">C</span>om
           </nav>
@@ -15,8 +15,16 @@ const NavBar = () => {
            <button className="nav hamburger-menu  mt-[12px] ">
             <FontAwesomeIcon className='h-[1.75em] text-slate-300' icon={faBars }/></button>  
         </nav>
-        </navbar>
- 
+       </navbar>
+       <ul className="navlinks text-[#fff] mt-[0.75em] font-[400] bg-[#131313] 
+            text-[1em]  ">
+              <li className="navlink">Home </li>
+              <li className="navlink">About</li>
+              <li className="navlink">Services</li>
+              <li className="navlink">Pricing</li>
+              <li className="navlink">Pages</li>
+              <li className="navlink">Contact</li>
+            </ul> 
     </section>
   )
 }
